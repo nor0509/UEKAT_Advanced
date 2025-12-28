582cdbf2ef663eb66f8c5337c5238d50286661c3
nor0509 <norbert.kabzinski@proton.me>
2025-12-28 03:57:50 +0100

    feat: implement SQLite database and file upload and url downloads endpoint

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
 import json
 import os
 import uuid
-
+import sqlite3
 import pika
-from flask import Flask, jsonify
+import requests
+from flask import Flask, jsonify, request
+
 
 app = Flask(__name__)
 
 BASE_DIR = "/app/test_photo"
-RABBITMQ_HOST = 'rabbitmq'
-QUEUE_NAME = 'task_queue'
+RABBITMQ_HOST = "rabbitmq"
+QUEUE_NAME = "task_queue"
+DB_PATH = "/app/db/tasks.db"
+
 
 def send_to_queue(task_data):
     connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
@@ -17,18 +21,18 @@ def send_to_queue(task_data):
     channel.queue_declare(queue=QUEUE_NAME, durable=True)
 
     channel.basic_publish(
-        exchange='',
+        exchange="",
         routing_key=QUEUE_NAME,
-        body=json.dumps(task_data), # tu przekazuje json z requesta
+        body=json.dumps(task_data),  # tu przekazuje json z requesta
         properties=pika.BasicProperties(
-            delivery_mode=2, # tutaj zeby zapisac w rabbit jakby wywalilo
-        ))
+            delivery_mode=2,  # tutaj zeby zapisac w rabbit jakby wywalilo
+        ),
+    )
     connection.close()
 
 
-@app.route('/process/<filename>', methods=['GET'])
+@app.route("/process/<filename>", methods=["GET"])
 def process_request(filename):
-
 
     full_path = os.path.join(BASE_DIR, filename)
 
@@ -36,26 +40,157 @@ def process_request(filename):
         return jsonify({f"error": f"Plik nie istnieje: {full_path}"}), 404
 
     task_id = str(uuid.uuid4())
-    message = {
-        "task_id": task_id,
-        "filename": filename
-    }
+    message = {"task_id": task_id, "full_path": full_path}
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    sql = "INSERT INTO tasks (task_id, status, filename) VALUES (?, ?, ?)"
+    values = (task_id, "PENDING", filename)
+    cursor.execute(sql, values)
+    conn.commit()
+    conn.close()
 
     try:
         send_to_queue(message)
 
-        return jsonify({
-            "message": "Zadanie przyjęte do realizacji",
-            "task_id": task_id,
-            "status": "queued"
-        }), 202
+        return jsonify({"message": "Zadanie przyjęte do realizacji", "task_id": task_id, "status": "queued"}), 202
     except Exception as e:
         return jsonify({"error": f"Błąd kolejki: {str(e)}"}), 500
+
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
 
+@app.route("/tasks/<task_id>", methods=["GET"])
+def view_task(task_id):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    sql = "SELECT task_id, status, result, filename, created_at from tasks where task_id = ?"
+    value = (task_id,)
+    cursor.execute(sql, value)
+    row = cursor.fetchone()
+    conn.close()
+
+    if row:
+        data = {"task_id": row[0], "status": row[1], "persons_detected": row[2], "file_name": row[3]}
+        return jsonify(data)
+    return jsonify({"error": "Task not found"}), 404
+
+
+@app.route("/tasks/all", methods=["GET"])
+def view_all_tasks():
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    sql = "SELECT task_id, status, result, filename, created_at from tasks"
+    cursor.execute(sql)
+    rows = cursor.fetchall()
+    conn.close()
+    data = []
+    for row in rows:
+        if row:
+            task = {"task_id": row[0], "status": row[1], "persons_detected": row[2], "file_name": row[3]}
+            data.append(task)
+    if data is None:
+        return jsonify({"error": "Brak danych w bazie danych"})
+    return jsonify(data)
+
+
+@app.route("/api/count_people", methods=["POST"])
+def count_people_get():
+    img_url = request.args.get("url")
+
+    if not img_url:
+        return jsonify({"error": "Musisz podać parametr url"}), 400
+
+    try:
+        response = requests.get(img_url, timeout=10)
+        task_id = str(uuid.uuid4())
+        filename = f"url_{task_id}.jpg"
+        full_path = os.path.join("/app/uploads", filename)
+
+        with open(full_path, "wb") as f:
+            f.write(response.content)
+    except Exception as e:
+        return jsonify({"error": f"Błąd pobierania: {e}"}), 400
+
+    message = {"task_id": task_id, "full_path": full_path}
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    sql = "INSERT INTO tasks (task_id, status, filename) VALUES (?, ?, ?)"
+    values = (task_id, "PENDING", filename)
+    cursor.execute(sql, values)
+    conn.commit()
+    conn.close()
+
+    try:
+        send_to_queue(message)
+
+        return jsonify({"message": "Zadanie przyjęte do realizacji", "task_id": task_id, "status": "queued"}), 202
+    except Exception as e:
+        return jsonify({"error": f"Błąd kolejki: {str(e)}"}), 500
+
+
+@app.route("/send_image", methods=["POST"])
+def send_image():
+    if "file" not in request.files:
+        return jsonify({"error": "Brak części 'file' w żądadniu"}), 400
+
+    file = request.files["file"]
+
+    if file.filename == "":
+        return jsonify({"error": "Nie wybrano pliku"})
+
+    if file:
+        try:
+            ext = os.path.splitext(file.filename)[1]
+            if not ext:
+                ext = ".jpg"
+
+            task_id = str(uuid.uuid4())
+            safe_filename = f"upload_{task_id}{ext}"
+
+            upload_dir = "/app/uploads"
+            os.makedirs(upload_dir, exist_ok=True)
+
+            full_path = os.path.join(upload_dir, safe_filename)
+
+            file.save(full_path)
+
+            conn = sqlite3.connect(DB_PATH)
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO tasks (task_id, status, filename) VALUES (?, ?, ?)", (task_id, "PENDING", safe_filename))
+            conn.commit()
+            conn.close()
+
+            message = {"task_id": task_id, "full_path": full_path}
+            send_to_queue(message)
+
+            return jsonify({"message": "Plik przesłany i zakolejkowany", "task_id": task_id, "filename": safe_filename}), 202
+        except Exception as e:
+            return jsonify({"error": f"Błąd podczas zapisu: {str(e)}"}), 500
+
+
+def init_db():
+    os.makedirs("/app/db", exist_ok=True)
+
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS tasks (
+            task_id TEXT PRIMARY KEY,
+            status TEXT,
+            result INTEGER,
+            filename TEXT,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+    """
+    )
+    conn.commit()
+    conn.close()
+
+
 if __name__ == "__main__":
+    init_db()
     app.run(debug=True, host="0.0.0.0", port=5000)
--- worker.py
+++ worker.py
@@ -1,28 +1,28 @@
 import json
 import os
+import sqlite3
 import time
 from detector import detect_objects
 import pika
 
 
-
-
-
 print("Czekam 10 sekund na start RabbitMQ...", flush=True)
 time.sleep(10)
 
-#stałe
-BASE_DIR = "/app/test_photo"
+# stałe
+BASE_DIR = "/app/uploads"
 OUTPUT_DIR = "/app/processed"
-RABBITMQ_HOST = 'rabbitmq'
-QUEUE_NAME = 'task_queue'
+RABBITMQ_HOST = "rabbitmq"
+QUEUE_NAME = "task_queue"
+DB_PATH = "/app/db/tasks.db"
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host = RABBITMQ_HOST))
+connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQ_HOST))
 channel = connection.channel()
 
 channel.queue_declare(queue=QUEUE_NAME, durable=True)
 
-def callback(ch, method, properties, body ):
+
+def callback(ch, method, properties, body):
     """
     # ch: kanał
     # method: informacje o dostarczeniu (np. tag)
@@ -31,22 +31,33 @@ def callback(ch, method, properties, body ):
     """
 
     data = json.loads(body)
-    filename = data['filename']
-    task_id = data.get('task_id', None)
+    full_path = data["full_path"]
+    task_id = data.get("task_id", None)
 
-    print(f"Odebrano zadanie dla pliku {filename} (ID: {task_id}", flush=True)
+    print(f"Odebrano zadanie dla pliku {full_path} (ID: {task_id})", flush=True)
 
-    input_path = os.path.join(BASE_DIR, filename)
-    output_filename = f"processed_{task_id}_{filename}"
+    output_filename = f"processed_{task_id}.jpg"
     output_path = os.path.join(OUTPUT_DIR, output_filename)
 
     try:
-        count = detect_objects(input_path, output_path)
+        count = detect_objects(full_path, output_path)
         print(f"Zakończono. Znaleziono osób: {count}", flush=True)
+
+        conn = sqlite3.connect(DB_PATH)
+        cursor = conn.cursor()
+
+        sql = "UPDATE tasks SET status = ?, result = ? WHERE task_id = ?"
+        values = ("COMPLETED", count, task_id)
+
+        cursor.execute(sql, values)
+        conn.commit()
+        conn.close()
+
     except Exception as e:
-        print(f"Błąd podczas realizacji procesu detect_objects:{e}", flush = True)
+        print(f"Błąd podczas realizacji procesu detect_objects:{e}", flush=True)
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
-channel.basic_qos(prefetch_count=1) #przyjmujemy 1 msg do pracy
-channel.basic_consume(queue=QUEUE_NAME, on_message_callback=callback) #jak dostajemy msg to odpal callback
-channel.start_consuming() #uruchamiamy petle
+
+channel.basic_qos(prefetch_count=1)  # przyjmujemy 1 msg do pracy
+channel.basic_consume(queue=QUEUE_NAME, on_message_callback=callback)  # jak dostajemy msg to odpal callback
+channel.start_consuming()  # uruchamiamy petle
